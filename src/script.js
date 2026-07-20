ec09dea7f07e96004fab0e9c51bc4bc379e92955
unknown <lampteylinda97@gmail.com>
2026-07-20 13:08:55 -0700

    Adding alert message

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 // Taking input from the user and storing it in a variable called userName.
 let userName = prompt("Enter your name");
+alert(userName);
 let userAge = prompt("Enter your age");
 let userEmail = prompt("Enter your email");
 let message = `Hello ${userName}, you are ${userAge} years old and your email is ${userEmail}.`;
+alert(message);
