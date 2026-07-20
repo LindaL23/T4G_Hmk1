0a7c9afa2da1ca7f4d9907b3c52232e1aca60f9f
unknown <lampteylinda97@gmail.com>
2026-07-20 13:04:51 -0700

    Adding an src folder

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// Taking input from the user and storing it in a variable called userName.
+let userName = prompt("Enter your name");
+let userAge = prompt("Enter your age");
+let userEmail = prompt("Enter your email");
+let message = `Hello ${userName}, you are ${userAge} years old and your email is ${userEmail}.`;
